85793231f08ae6f3892cf441f06590c76c0f49b4
Sean Pollock <seanpollock3344@gmail.com>
2017-03-01 23:04:35 -0600

    More mutations...

--- server.js
+++ server.js
@@ -2,83 +2,69 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQ: schema language
 var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
-    getDie(numSides: Int): RandomDie
-    getMessage: String
-  }
-  type Mutation {
-    setMessage(message: String): String
+  input MessageInput {
+    content: String
+    author: String
   }
 
+  type Message {
+    id: ID!
+    content: String
+    author:String
+  }
+
+  type Query {
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+  }
 `);
 
-
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
 }
 
 var fakeDatabase = {};
-// The root provides a clear resolver function for each API endpoint
-var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
-  },
-  rollDice: ({numDice, numSides}) => {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
-  },
-  getDie: ({numSides}) => {
-    return new RandomDie(numSides || 6);
-  },
-  setMessage: ({message}) => {
-    fakeDatabase.message = message;
-    return message;
-  },
-  getMessage: () => {
-    return fakeDatabase.message;
-  }
 
+var root = {
+  getMessage: ({id}) => {
+    if(!fakeDatabase[id]) {
+      throw new Error('no message exists with' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+
+  createMessage: ({id}) => {
+    var id = require('crypto').randomBytes(10).String('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input)
+  },
+
+  updateMessage: ({id, input}) => {
+    if(!fakeDatabase[id]) {
+      throw new Error('no message exists with' + id);
+    }
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+    }
 }
 
 var app = express();
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
-}));
-app.listen(4000);
-console.log('server listening on port 4000');
+}))
+app.listen(5000);
+
+console.log('listeningn on 5000')
